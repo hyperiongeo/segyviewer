2095382af6ee3b2d3c395e0c0742ab55e0eacaac
Hyperion <37962713+hyperiongeo@users.noreply.github.com>
2020-03-17 19:32:20 -0600

    update

--- applications/segyviewer.bat
+++ applications/segyviewer.bat
@@ -3,6 +3,7 @@ rem python segyviewer %1
 
 
 rem python segyviewer "H:\Seisware\Blackfoot_RQA\blackfoot\stkBFoot5DMig2.sgy"
-rem python segyviewer "../tests/testdata/small.sgy"
-python segyviewer "small.sgy"
+python segyviewer "../tests/testdata/small.sgy"
+rem python segyviewer "small.sgy"
+rem python segyviewer %1
 rem python segyviewer "H:\Seisware\Blackfoot_RQA\blackfoot\inverted3_BHN.sgy"
--- applications/segyviewer.ps1
+++ applications/segyviewer.ps1
@@ -5,5 +5,5 @@
 #python segyviewer "H:\Seisware\Blackfoot_RQA\blackfoot\stkBFoot5DMig2.sgy"
 # python segyviewer "../tests/testdata/small.sgy"
 #& python segyviewer "small.sgy"
-#& python segyviewer $args[0]
-& python segyviewer "H:\Seisware\Blackfoot_RQA\blackfoot\stkBFoot5DMig2.sgy"
+& python segyviewer $args[0]
+#& python segyviewer "H:\Seisware\Blackfoot_RQA\blackfoot\stkBFoot5DMig2.sgy"
